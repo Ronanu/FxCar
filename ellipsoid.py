d8b825d67a70b0efda703a74b2b4068b833eecd9
Fx <felixrodlsberger@gmail.com>
2024-04-28 10:18:51 +0200

    little chanes

--- ellipsoid.py
+++ ellipsoid.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 # Parameter für die gestauchte Ellipse
 a = 3  # Länge der horizontalen Achse
 b = 2  # Länge der vertikalen Achse
-h = 0.2  # Horizontaler Verschiebungsfaktor
+h = 1  # Horizontaler Verschiebungsfaktor
 
 # Parametrische Darstellung der Ellipse
 t = np.linspace(0, 2*np.pi, 100)
@@ -12,12 +12,15 @@ x = a * np.cos(t)
 y = b * np.sin(t) * (h * np.cos(t)+1)
 
 ellipsenpunkte = np.array([x, y])
+# plot the coordinates scaled the same
+
 # Plotten der gestauchten Ellipse
-plt.figure(figsize=(8, 6))
-plt.plot(ellipsenpunkte[0], ellipsenpunkte[1])
-# plt.gca().set_aspect('equal', adjustable='box')  # Das Seitenverhältnis beibehalten
+plt.figure(figsize=(8, 8))
+plt.plot(*ellipsenpunkte)
+plt.gca().set_aspect('equal', adjustable='box')  # Das Seitenverhältnis beibehalten
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('Asymmetrisch gestauchte Ellipse') 
 plt.grid(True)
 plt.show()
+# 
