fd4352e020a03d8de300a9c988c87b52174b6824
Fx <felixrodlsberger@gmail.com>
2024-10-12 18:06:01 +0200

    faces...

--- SrfaceEvolver.py
+++ SrfaceEvolver.py
@@ -1,10 +1,9 @@
 import numpy as np
 from scipy.spatial import Delaunay
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 class SurfaceEvolverInput:
-    def __init__(self, rand, num_r=15, num_phi=360):
+    def __init__(self, rand, num_r=10, num_phi=36):
         self.rand = rand
         self.num_radii = num_r  # Anzahl der Radialpunkte
         self.num_angles = num_phi  # Anzahl der Winkelpunkte
@@ -62,49 +61,45 @@ class SurfaceEvolverInput:
             vertex_mapping[i] = vertex_id
             vertex_id += 1
 
-        # Faces (Dreiecke)
-        evolver_input.append("\nfaces\n")
+        # Edges und Faces basierend auf der Delaunay-Triangulation
+        evolver_input.append("\nedges\n")
+        edge_mapping = {}
+        edge_id = 1
         for simplex in triangulation.simplices:
-            v1 = vertex_mapping[simplex[0]]
-            v2 = vertex_mapping[simplex[1]]
-            v3 = vertex_mapping[simplex[2]]
-            evolver_input.append(f"{v1} {v2} {v3}\n")
+            for k in range(3):
+                v1 = simplex[k]
+                v2 = simplex[(k + 1) % 3]
+                edge_key = (v1, v2)
+                # Kante nur einmal definieren
+                if edge_key not in edge_mapping and (v2, v1) not in edge_mapping:
+                    evolver_input.append(f"{edge_id} {vertex_mapping[v1]} {vertex_mapping[v2]}\n")
+                    edge_mapping[edge_key] = edge_id
+                    edge_id += 1
+
+        # Faces
+        evolver_input.append("\nfaces\n")
+        face_id = 1
+        for simplex in triangulation.simplices:
+            v1, v2, v3 = simplex
+            edge1 = edge_mapping.get((v1, v2)) or edge_mapping.get((v2, v1))
+            edge2 = edge_mapping.get((v2, v3)) or edge_mapping.get((v3, v2))
+            edge3 = edge_mapping.get((v3, v1)) or edge_mapping.get((v1, v3))
+            evolver_input.append(f"{face_id} {edge1} {edge2} {edge3}\n")
+            face_id += 1
 
         return ''.join(evolver_input)
 
     def visualize_surface(self):
-        if self.Z_init is None:
-            self.calculate_initial_surface()
-
+        """Visualisiert die Oberfläche zur Überprüfung der Delaunay-Triangulation."""
         X = self.R * np.cos(self.Phi) + self.rand.center_x
         Y = self.R * np.sin(self.Phi) + self.rand.center_y
-        Z = self.Z_init
 
-        vertices = np.column_stack((X.ravel(), Y.ravel(), Z.ravel()))
-        triangulation = Delaunay(vertices[:, :2])  # Nur (X, Y)-Koordinaten für die Delaunay-Triangulation
-
-        # 3D-Plot erzeugen
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-
-        # Hinzufügen der Dreiecke zur Oberfläche
-        faces = vertices[triangulation.simplices]
-        surface = Poly3DCollection(faces, alpha=0.6, edgecolor='k')
-        ax.add_collection3d(surface)
-
-        # Achsenbeschriftungen
-        ax.set_xlabel('X')
-        ax.set_ylabel('Y')
-        ax.set_zlabel('Z')
-        ax.set_title('Visualisierung der generierten Oberfläche')
-
-        # Grenzen setzen
-        ax.auto_scale_xyz([vertices[:, 0].min(), vertices[:, 0].max()],
-                          [vertices[:, 1].min(), vertices[:, 1].max()],
-                          [vertices[:, 2].min(), vertices[:, 2].max()])
-
+        plt.triplot(X.ravel(), Y.ravel(), Delaunay(np.column_stack((X.ravel(), Y.ravel()))).simplices)
+        plt.plot(X, Y, 'o')
+        plt.title("Delaunay Triangulation Visualization")
+        plt.xlabel("X")
+        plt.ylabel("Y")
         plt.show()
-
 
 # Beispielaufruf der Klasse
 if __name__ == "__main__":
@@ -115,12 +110,14 @@ if __name__ == "__main__":
     # Initialisiere das Rand-Objekt mit den eingelesenen Punkten
     rand = Rand(points, interpolation_type='cubic')
     surface_input_generator = SurfaceEvolverInput(rand)
+    
+    # Visualisierung zur Überprüfung
+    surface_input_generator.visualize_surface()
+
+    # Generieren der Evolver Input-Datei
     evolver_input_text = surface_input_generator.generate_surface_evolver_input()
 
     # Speichern des Inputs in eine Datei
     with open("surface_evolver_input.fe", "w") as file:
         file.write(evolver_input_text)
     print("Surface Evolver Input erfolgreich generiert.")
-
-    # Visualisierung der Oberfläche
-    surface_input_generator.visualize_surface()
